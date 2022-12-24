e4c1d94af8656aac71b129ed074dfea099e0ba61
Prathamesh Kolhe <pratham2002522@gmail.com>
2022-12-25 00:48:09 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5005', {
+  const response = await fetch('https://cbot-it.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
